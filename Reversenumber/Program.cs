b8d15624173ffaef6abb937e4480df2eade3dd9a
subbareddyalc21 <subbareddyalc21@gmail.com>
2021-06-13 08:43:46 +0530

    reverse number class added

---
+++ Reversenumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CSharpProgram
+{
+    class Program
+    {
+        // Main function, execution entry point of the program  
+        static void Main(string[] args) // string type parameters  
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
